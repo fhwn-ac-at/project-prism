295d935bf382c62f768b017292e9375f5f67c99b
Clemens <clemens.niklos@gmail.com>
2025-01-24 18:38:26 +0100

    Fixed merge conflict

--- Frontend/src/app/networking/services/api/api.service.ts
+++ Frontend/src/app/networking/services/api/api.service.ts
@@ -2,7 +2,6 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ConfigService } from '../../../services/config/config.service';
 import { map, Observable } from 'rxjs';
-import { ConnectToLobbyResponse, isConnectToLobbyResponse } from './dto/ConnectToLobbyResponse';
 import { isUser } from '../../dtos/shared/User.guard';
 import { User } from '../../dtos/shared/User';
 

939ee933e747bb382079f38226c44657b1b8c802
R41f_K <ralf.kuehmayer@gmail.com>
2025-01-27 14:22:54 +0100

    cleanup

--- MessageLib/CustomJSchemaResolver.cs
+++ MessageLib/CustomJSchemaResolver.cs
@@ -31,42 +31,5 @@
             this.logger?.LogWarning("Schema not known to CustomJSchemaResolver");
             return null;
         }
-
-        public override JSchema? GetSubschema(SchemaReference reference, JSchema rootSchema)
-        {
-            if (reference.SubschemaId==null||!reference.SubschemaId.OriginalString.StartsWith("#"))
-            {
-                this.logger?.LogWarning("Invalid subschema id");
-                return null;
-            }
-
-            string[] pathPices = reference.SubschemaId.OriginalString[2..].Split("/");
-
-            if (pathPices.Length<1)
-            {
-                this.logger?.LogWarning("Invalid subschema id");
-                return null;
-            }
-
-            if (!rootSchema.ExtensionData.TryGetValue(pathPices[0], out JToken? node)||node==null)
-            {
-                this.logger?.LogWarning("Subschema {} not in schema {}", pathPices[0], rootSchema.ToString());
-                return null;
-            }
-
-            for (int i = 1; i<pathPices.Length; i++)
-            {
-                JToken? newNode = node.SelectToken(pathPices[i]);
-                if (newNode==null)
-                {
-                    this.logger?.LogWarning("Subschema {} not in schema {}", pathPices[i], node.ToString());
-                    return null;
-                }
-
-                node=newNode;
-            }
-
-            return JSchema.Parse(node.ToString());
-        }
     }
 }
